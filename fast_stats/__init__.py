5e40effb86fcb4ec5cddcbd195cf6b742912f508
Zachary Coleman <zacharywcoleman@gmail.com>
2022-08-21 09:53:50 -0600

    adding module level docstring

--- fast_stats/__init__.py
+++ fast_stats/__init__.py
@@ -1,3 +1,15 @@
+"""
+`fast-stats` is a fast and simple library for calculating basic statistics such as:
+precision, recall, and f1-score. The library also supports the calculation of confusion
+matrices. For examples, please look at the `examples/` folder.
+
+The project was developed using the maturin framework.
+
+The library's main functions are:
+- binary.binary_stats
+- multiclass.stats
+"""
+
 from .binary import (
     binary_f1_score,
     binary_precision,
